4bf06bfee91f9f05f228b8c1166d7689436dff8e
jesse <jesse.frazier.8@gmail.com>
2025-06-10 23:29:13 -0400

    disable capslock completely, also pgrep for pipewire instead of killing

--- awesomestart.sh
+++ awesomestart.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 picom --backend xrender &
-setxkbmap -option caps:escape
+setxkbmap -option caps:none
 nitrogen --restore &
 
 if [ -f /usr/bin/gentoo-pipewire-launcher ]; then
-	killall pipewire; gentoo-pipewire-launcher &
+	pgrep pipewire || gentoo-pipewire-launcher &
 fi
 
 udiskie &
