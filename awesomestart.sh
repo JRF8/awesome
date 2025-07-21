dc729e2090aba204a7c44e6d6e92df3bbde8fbbc
jesse <jesse.frazier.8@gmail.com>
2025-07-21 11:32:18 -0400

    load xresources on startup

--- awesomestart.sh
+++ awesomestart.sh
@@ -3,6 +3,8 @@
 picom --backend xrender &
 setxkbmap -option caps:none
 nitrogen --restore &
+xrdb ~/.Xresources
+xrdb ~/.cache/wal/colors.Xresources
 
 if [ -f /usr/bin/gentoo-pipewire-launcher ]; then
 	pgrep pipewire || gentoo-pipewire-launcher &
