306fdacc4807e698f8224119cb0ace28ada9530d
jesse <jesse.frazier.8@gmail.com>
2025-08-12 20:31:46 -0400

    small change to xresources loading

--- awesomestart.sh
+++ awesomestart.sh
@@ -4,10 +4,10 @@ picom --backend xrender &
 setxkbmap -option caps:none
 nitrogen --restore &
 xrdb ~/.Xresources
-xrdb ~/.cache/wal/colors.Xresources
 
 if [ -f /usr/bin/gentoo-pipewire-launcher ]; then
 	pgrep pipewire || gentoo-pipewire-launcher &
 fi
 
 pgrep udiskie || udiskie &
+
