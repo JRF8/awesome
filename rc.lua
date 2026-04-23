0f64358a51bb61b1120c1ab9c7dcfbc5ea496057
jesse <jesse.frazier.8@gmail.com>
2026-04-22 22:23:07 -0400

    new favorite terminal: kitty

--- rc.lua
+++ rc.lua
@@ -77,7 +77,7 @@ end
 beautiful.init("~/.config/awesome/themes/" .. "default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "alacritty"
+terminal = "kitty"
 editor = os.getenv("EDITOR") or "editor"
 editor_cmd = terminal .. " -e " .. editor
 
